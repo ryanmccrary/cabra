5996ec2175e82478ba45eab1a8940b57cecd2458
Ryan McCrary <rrmccrary@gmail.com>
2014-05-20 17:01:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cabra::Application.config.secret_key_base = 'e6a716b6b2b8b5720675eb76571dd022a23a18e3283f457d415d7c1faf4d72c709f70bf8c188ebed37bb3dfef9d98d4b5f79a98b7cee8f9039922809b54a2df8'
