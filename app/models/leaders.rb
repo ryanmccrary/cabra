0e075d786f11e03fc70f8a25ad6bc9891ada1529
Ryan McCrary <rrmccrary@gmail.com>
2014-05-20 17:07:13 -0400

    Added leader model

---
+++ app/models/leaders.rb
@@ -0,0 +1,2 @@
+class Leaders < ActiveRecord::Base
+end
---
+++ test/models/leaders_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LeadersTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
