643fadafeb7354fecbd894874b166ff28d1a3fa6
Ryan McCrary <rrmccrary@gmail.com>
2014-05-20 21:02:56 -0400

    Added info to trip migration

--- db/migrate/20140521005845_create_trips.rb
+++ db/migrate/20140521005845_create_trips.rb
@@ -1,6 +1,13 @@
 class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
+      t.integer :trip_type
+      t.integer :location
+      t.integer :group
+      t.string :pickup_time
+      t.string :dropoff_time
+      t.datetime :date
+      t.boolean :pickup
 
       t.timestamps
     end
