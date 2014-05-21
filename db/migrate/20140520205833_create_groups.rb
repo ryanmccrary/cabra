239636a9e26f5162c5c3a9961f467b8c89f53876
Ryan McCrary <rrmccrary@gmail.com>
2014-05-20 20:20:44 -0400

    Added info to groups migration

--- db/migrate/20140520205833_create_groups.rb
+++ db/migrate/20140520205833_create_groups.rb
@@ -1,8 +1,16 @@
 class CreateGroups < ActiveRecord::Migration
   def change
     create_table :groups do |t|
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip
+
 
       t.timestamps
     end
+
+    add_index :groups, :name, unique: true
   end
 end
