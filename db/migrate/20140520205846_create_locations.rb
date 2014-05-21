9395458b5cbb6d0de772cb4654d6c9b9d87ad54f
Ryan McCrary <rrmccrary@gmail.com>
2014-05-20 20:23:58 -0400

    Added info to locations migration

--- db/migrate/20140520205846_create_locations.rb
+++ db/migrate/20140520205846_create_locations.rb
@@ -1,6 +1,12 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.integer :type_id
 
       t.timestamps
     end
