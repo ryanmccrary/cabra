c58b0a24c90b7d5ab9697cd872b21a17313a29e8
Ryan McCrary <rrmccrary@gmail.com>
2014-05-20 20:25:17 -0400

    Added info to trip_types migration

--- db/migrate/20140520210212_create_trip_types.rb
+++ db/migrate/20140520210212_create_trip_types.rb
@@ -1,8 +1,11 @@
 class CreateTripTypes < ActiveRecord::Migration
   def change
     create_table :trip_types do |t|
+      t.string :name
 
       t.timestamps
     end
+
+    add_index :trip_types, :name, unique: true
   end
 end
