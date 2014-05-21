8e6d9ee86397d3f8b86f8d6c86235679d8166da9
Ryan McCrary <rrmccrary@gmail.com>
2014-05-21 13:50:27 -0400

    Added sent boolean to plan

--- db/migrate/20140520210435_create_plans.rb
+++ db/migrate/20140520210435_create_plans.rb
@@ -2,6 +2,7 @@ class CreatePlans < ActiveRecord::Migration
   def change
     create_table :plans do |t|
       t.string :unique_identifier
+      t.boolean :plan_sent, default: false
 
       t.timestamps
     end
