60454310427e3b00fc8fbaccb727f4fdd10629bd
Ryan McCrary <rrmccrary@gmail.com>
2014-05-20 20:56:14 -0400

    Added info to plans migration

--- db/migrate/20140520210435_create_plans.rb
+++ db/migrate/20140520210435_create_plans.rb
@@ -1,8 +1,11 @@
 class CreatePlans < ActiveRecord::Migration
   def change
     create_table :plans do |t|
+      t.string :unique_identifier
 
       t.timestamps
     end
+
+    add_index :unique_identifier
   end
 end
