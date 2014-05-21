58ae4f2f8215647b6136893c08bb0cbd399c9ae9
Ryan McCrary <rrmccrary@gmail.com>
2014-05-20 20:58:04 -0400

    Added info to leader migration

--- db/migrate/20140520210705_create_leaders.rb
+++ db/migrate/20140520210705_create_leaders.rb
@@ -1,6 +1,11 @@
 class CreateLeaders < ActiveRecord::Migration
   def change
     create_table :leaders do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :telephone
+      t.integer :group_id
 
       t.timestamps
     end
