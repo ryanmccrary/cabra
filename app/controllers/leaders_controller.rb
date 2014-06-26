1be100c0d82ad8699e65c9feb5cb09d0bbefa43c
Ryan McCrary <rrmccrary@gmail.com>
2014-06-26 13:43:48 -0400

    add leader form functioning and returning index

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -7,4 +7,19 @@ class LeadersController < ApplicationController
   def new
     @leader = Leader.new
   end
+  def create
+    @leader = Leader.new(leader_params)
+    if @leader.save
+      redirect_to leaders_path, notice: "Leader created succesfully"
+    else
+      render "new"
+    end
+  end
+
+    private
+
+    def leader_params
+      params.require(:leader).permit(:first_name, :last_name, :email,
+                                    :telephone, :group_id)
+    end
 end
