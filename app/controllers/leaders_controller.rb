3aa8e3e360176e5f25c3e4298dfe2be74c7c35e1
Ryan McCrary <rrmccrary@gmail.com>
2014-06-26 13:17:11 -0400

    Fixed model names, leader form framed, better errors added

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,2 +1,10 @@
 class LeadersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @leaders = Leader.all
+  end
+  def new
+    @leader = Leader.new
+  end
 end
--- app/models/leaders.rb
+++
@@ -1,3 +0,0 @@
-class Leaders < ActiveRecord::Base
-  belongs_to :group
-end
