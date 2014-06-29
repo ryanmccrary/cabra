fe4a3638c5cc2cf48e39a3d419024231231c15e9
Ryan McCrary <rrmccrary@gmail.com>
2014-06-29 11:17:17 -0400

    trips views

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Cabra::Application.routes.draw do
 
   resources :groups
   resources :leaders
+  resources :trips
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
