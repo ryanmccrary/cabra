6e632ea1feac940b792b56a7ddff3e926b81ee3c
Ryan McCrary <rrmccrary@gmail.com>
2014-07-01 15:31:40 -0400

    location controller

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,34 @@
+class LocationssController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @locations = Location.all
+  end
+  def new
+    @location = Location.new
+  end
+  def create
+    @location = Location.new(location_params)
+  end
+  def show
+    @location = Location.find(params[:id])
+  end
+  def edit
+    @location = Location.find(params[:id])
+  end
+  def update
+    @location = Location.find(params[:id])
+    if @location.update_attributes(location_params)
+      redirect_to locations_path, notice: @location.name + " successfully updated!"
+    else
+      render "edit"
+    end
+  end
+
+    private
+
+    def location_params
+      params.require(:location).permit(:name, :street, :city, 
+                                    :state, :zip, :type_id)
+    end
+end
