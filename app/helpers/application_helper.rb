2e7c1b86037db0f2e0bd88438da3231a7f924b7b
Ryan McCrary <rrmccrary@gmail.com>
2014-07-02 16:39:27 -0400

    added google static image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def is_active?(link_path)
     "active" if current_page?(link_path)
   end
+
+  def google_static_image(address)
+    image_tag "http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&sensor=false&markers=Greenville%20SC&size=500x300&markers=#{address}"
+  end
 end
