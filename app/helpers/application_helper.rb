061250da81f8ab4754ede63facfde0f7ae5c0aac
Ryan McCrary <rrmccrary@gmail.com>
2014-07-02 19:20:30 -0400

    added google_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     "active" if current_page?(link_path)
   end
 
+  def google_link_to(address)
+    link_to "#{address}", "http://maps.google.com/?q=#{address}"
+  end
+
   def google_static_image(address)
     image_tag "http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&sensor=false&markers=Greenville%20SC&size=500x300&markers=#{address}"
   end
