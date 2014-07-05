2ec59a152ad835ba90796d7566de6b870a7a32a4
Ryan McCrary <rrmccrary@gmail.com>
2014-07-05 10:51:51 -0400

    fixed location in trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -22,7 +22,7 @@ class TripsController < ApplicationController
   end
   def show
     @trip = Trip.find(params[:id])
-    @location = Location.find(@trip.location)
+    @location = Location.find(@trip.location_id)
     @group = Group.find(@trip.group)
   end
   def edit
