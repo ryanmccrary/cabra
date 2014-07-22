a24ad499291a00f52d8afe8e32fe5be79ff87cdb
Ryan McCrary <rrmccrary@gmail.com>
2014-07-21 21:26:32 -0400

    added before_create filter and new/create actions

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,7 +1,17 @@
 class PlansController < ApplicationController
   before_filter :authenticate_user!
-  
+
   def new
-    
+    @plan = Plan.new
   end
+  def create
+    @plan = Plan.new(plan_params)
+  end
+
+    private
+
+    def plan_params
+      params.require(:plan).permit(:unique_identifier, :group_id, 
+                                  :leader_id, :plan_sent)
+    end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -2,4 +2,13 @@ class Plan < ActiveRecord::Base
   belongs_to :group
   
   has_many :trips
+
+  before_create :create_unique_id
+
+  def create_unique_id
+    begin
+      self.unique_identifier = SecureRandom.urlsafe_base64(6).downcase
+    end while self.class.exists?(:unique_identifier => unique_identifier)
+  end
+  
 end
