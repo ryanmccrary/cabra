0cd7cf416f08a6aa7749336f13fc4fbb8e58055e
Ryan McCrary <rrmccrary@gmail.com>
2014-07-30 16:55:36 -0400

    custom route for adding trips to plans

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Cabra::Application.routes.draw do
   resources :trips
   resources :activities
   resources :plans
+
+  get 'plans_controller/add_trips'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
