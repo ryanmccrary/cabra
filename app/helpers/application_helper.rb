9b4c25e14a85535df4a55b339d11837c7aeca901
rrmccrary@gmail.com <rrmccrary@gmail.com>
2015-02-12 12:19:17 -0500

    Moved time.parse to app helper and fixed in trip and plan show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
   def google_static_image(address)
     image_tag "http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&sensor=false&markers=Greenville%20SC&size=500x300&markers=#{address}"
   end
+
+  def string_to_time(input)
+    Time.parse(input).strftime("%l:%M %p")
+  end
 end
