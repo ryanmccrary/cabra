ecac1fc12664cedb5a802d6002613cfd0fc135de
Ryan McCrary <rrmccrary@gmail.com>
2017-01-26 21:41:53 -0500

    Add user roles to model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,6 +2,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
+    if user.has_role?(:admin)
+      can :manage, :all
+    else
+      cannot :create, Trip
+      can :read, Trip
+
+    end
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
