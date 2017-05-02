58330554841ddb36456e6c0cb1fa232b4360b9ce
Ryan McCrary <rrmccrary@gmail.com>
2017-05-02 08:05:56 -0400

    Add ability for admin to edit user roles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,21 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to users_path, notice: @user.email + " successfully updated!"
+    else
+      render "edit"
+    end
+  end
 
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :roles)
+    end
 end
