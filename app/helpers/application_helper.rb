c999e0ffe47170daef8c7e861bea09708b12124c
Ryan McCrary <rrmccrary@gmail.com>
2018-03-14 22:09:10 -0400

    fix google map link and add api key

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,13 @@ module ApplicationHelper
   end
 
   def google_static_image(address)
-    image_tag "http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&sensor=false&markers=Greenville%20SC&size=500x300&markers=#{address}"
+    # cleanaddress = address.gsub(/ /, '%20')
+    image_tag "http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&sensor=false \
+    &center=Greenville%20SC&size=500x300 \
+    &markers=color:green%7CGreenville%20SC \
+    &markers=color:red%7Clabel:G%7C#{address} \
+    &key=#{ENV['GOOGLE_MAPS_API_KEY']}
+"
   end
 
   def string_to_time(input)
