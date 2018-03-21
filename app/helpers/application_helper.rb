41860dac94bd1f87b914e6beb4e7b40bc762c933
Ryan McCrary <rrmccrary@gmail.com>
2018-03-20 20:52:41 -0400

    refactor google methods out of application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,20 +3,6 @@ module ApplicationHelper
     "active" if current_page?(link_path)
   end
 
-  def google_link_to(address)
-    link_to "#{address}", "http://maps.google.com/?q=#{address}"
-  end
-
-  def google_static_image(address)
-    # cleanaddress = address.gsub(/ /, '%20')
-    image_tag "http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&sensor=false \
-    &center=Greenville%20SC&size=500x300 \
-    &markers=color:green%7CGreenville%20SC \
-    &markers=color:red%7Clabel:G%7C#{address} \
-    &key=#{ENV['GOOGLE_MAPS_API_KEY']}
-"
-  end
-
   def string_to_time(input)
     Time.parse(input).strftime("%l:%M %p")
   end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -6,15 +6,22 @@ class Trip < ActiveRecord::Base
 
   has_one :report
 
+  delegate :full_address, to: :location
+
   scope :future, -> { where("date > ?", Date.today) }
-
   scope :past, -> { where("date <= ?", Date.today) }
-
   scope :year, -> { where("extract(year from date) = ?", Date.today.year) }
-
   scope :need_report, -> { select { |trip| trip.report.blank? } }
-
   scope :adding_trips, -> { where("plan_id IS ?", nil) }
 
   validates_presence_of :date, :group_id, :location_id, :activity_id, :pickup_time, :dropoff_time
+
+
+  def google_static_image
+    "http://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&sensor=false \
+    &center=Greenville%20SC&size=500x300 \
+    &markers=color:green%7CGreenville%20SC \
+    &markers=color:red%7Clabel:G%7C#{self.full_address} \
+    &key=#{ENV['GOOGLE_MAPS_API_KEY']}"
+  end
 end
